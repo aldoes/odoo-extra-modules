a3bf7c5823c166564bf52655bf2cea3f0a955810
Aldo Escobar <aldo@serimax.com.py>
2024-02-19 21:20:14 -0300

    [IMP] hr_expense_views : chenge visibility of vehicle and odometer, add default context

--- hr_expense_ext/models/hr_expense.py
+++ hr_expense_ext/models/hr_expense.py
@@ -1,6 +1,7 @@
 from datetime import date
 from odoo import api, fields, models, _
 
+
 class HrExpense(models.Model):
     _inherit = "hr.expense"
 
@@ -21,10 +22,11 @@ class HrExpense(models.Model):
     odometer = fields.Float(string="Odometer")
     product_is_fuel = fields.Boolean(compute='_compute_is_fuel')
     
-    def _compute_is_fuel(self):
-        #TODO - Verificar si es fuel
-        self.product_is_fuel= False
+    @api.onchange('product_id')
+    def _compute_is_fuel(self): 
+        for expense in self:   
+            fuel = self.env["cost.fleet.vehicle.fuel"].search([]).filtered(lambda r: r.product_id.id == self.product_id.id)
+            expense.product_is_fuel= bool(fuel.product_id.id)
 
 
-    #TODO - Si es pago de combustible habilitar el [CHAPA] automovil
-    #TODO - Si es combustible calcular precio unitario
+   #TODO - Si es combustible calcular precio unitario
